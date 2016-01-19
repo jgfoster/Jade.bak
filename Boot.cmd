121d1e6a52b6c0381a543ec64b837b66dfa2edfa
James Foster <github@jgfoster.net>
2016-01-18 20:02:36 -0800

    Changes to match evolving Dolphin 7 process for building a development image.

---
+++ Boot.cmd
@@ -0,0 +1,4 @@
+@ECHO OFF
+cd ..
+start Dolphin7 DPRO.img7 -q -i Jade\JadeDev -d . -f Jade\Boot.st 
+cd Jade
--- BootJade.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-for %%I in (..\Boot.st ..\DBOOT.img7 ..\DBOOT.sml) do (copy %%I .)
-..\Dolphin7 DBOOT.img7 Jade "Jade\sources\Jade Development.pax"
-IF %ERRORLEVEL% NEQ 0 (
-  ECHO Boot failed, Code=%ERRORLEVEL%
-)
-del Boot.st DBOOT.*
