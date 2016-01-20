4513e2958694b70bfc6e3b9c4a4b9a84b18219c0
James Foster <github@jgfoster.net>
2016-01-19 17:08:49 -0800

    Jade should boot against the current Dolphin 7.

--- Boot.cmd
+++ Boot.cmd
@@ -1,4 +1,5 @@
 @ECHO OFF
 cd ..
-start Dolphin7 DPRO.img7 -q -i Jade\JadeDev -d . -f Jade\Boot.st 
+Dolphin7 DPRO.img7 -q -i Jade\JadeDev -d . -x
 cd Jade
+start ..\Dolphin7 JadeDev.img7 -q -f Boot.st
