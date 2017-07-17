9ddc18f5c394aee60d8aa658888c81a78d00d72d
James Foster <github@jgfoster.net>
2017-07-17 14:24:12 -0700

    Cleanup boot scripts to avoid launch error (https://github.com/dolphinsmalltalk/Dolphin/issues/417).

--- BootJade.cmd
+++ BootJade.cmd
@@ -1,7 +1,2 @@
-@ECHO OFF
 del JadeDev.*
-cd ..
-Dolphin7 DPRO.img7 -q -i Jade\JadeDev -d . -x
-timeout /T 1 >nul
-cd Jade
-start ..\Dolphin7 JadeDev.img7 -q -f BootJade.st
+start ..\Dolphin7 ..\DPRO.img7 -q -i JadeDev -d . -f BootJade.st
